f779be5ef555b066690b8a4ae95073444cfc96f6
soliexe <sja.exe@gmail.com>
2021-02-25 03:31:35 +0330

    Change password functionality added.

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,5 +4,6 @@ from accounts import views
 app_name = 'accounts'
 
 urlpatterns = [
-    path('signup/', views.SignUpView.as_view(), name='signup')
+    path('signup/', views.SignUpView.as_view(), name='signup'),
+    path('change/password/', views.change_password, name='change_password')
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,10 @@
+from django.contrib import messages
+from django.contrib.auth import update_session_auth_hash
 from bootstrap_modal_forms.generic import BSModalCreateView
 from django.urls.base import reverse_lazy
 from accounts.forms import Registration
+from django.contrib.auth.forms import PasswordChangeForm
+from django.shortcuts import render, redirect
 
 
 class SignUpView(BSModalCreateView):
@@ -8,3 +12,21 @@ class SignUpView(BSModalCreateView):
     template_name = 'signup.html'
     success_message = 'Sign up succeeded.'
     success_url = reverse_lazy('index')
+
+
+def change_password(request):
+    if request.method == 'POST':
+        form = PasswordChangeForm(request.user, request.POST)
+        if form.is_valid():
+            user = form.save()
+            update_session_auth_hash(request, user)
+            messages.success(
+                request, 'Your password was successfully updated!')
+            return redirect('index')
+        else:
+            messages.error(request, 'Please correct the error below.')
+    else:
+        form = PasswordChangeForm(request.user)
+    return render(request, 'registration/password_change.html', {
+        'form': form
+    })
