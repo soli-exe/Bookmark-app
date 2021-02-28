3f1b3ed2edc790eb55006fcc70d279c1c7b6dc16
soliexe <sja.exe@gmail.com>
2021-02-28 04:02:19 +0330

    Email field has been changed to required field.

--- accounts/forms.py
+++ accounts/forms.py
@@ -6,6 +6,11 @@ from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 class Registration(PopRequestMixin, CreateUpdateAjaxMixin,
                    UserCreationForm):
 
+    def __init__(self, *args, **kwargs):
+        super(Registration, self).__init__(*args, **kwargs)
+        self.fields['email'].required = True
+        self.fields['phone'].required = False
+
     class Meta(UserCreationForm):
         model = CustomUser
         fields = UserCreationForm.Meta.fields + ('email', 'phone', )
