f2c576d5480c045f11148741aef68e73c7b81d48
soliexe <sja.exe@gmail.com>
2021-03-04 06:01:32 +0330

    Env file created

--- config/settings.py
+++ config/settings.py
@@ -12,10 +12,13 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
+from environs import Env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+env = Env()
+env.read_env()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
